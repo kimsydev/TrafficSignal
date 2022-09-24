ee5f482ca67451595fe3aec0d4bb7a113b5bdbf5
Kim DiPasqua <kimsydev@gmail.com>
2022-09-24 15:38:35 -0700

    Add files via upload
    updated file

--- trafficlight.js
+++ trafficlight.js
@@ -3,19 +3,21 @@ function startTrafficSignal () {
     let red=document.getElementById("red");
     let yellow=document.getElementById("yellow");
 
-    // green.style.opacity=1;
+    //yellow light stays on for 1 second
     setTimeout(function () {
         green.style.opacity=.3;
         red.style.opacity=.3;
         yellow.style.opacity=1;
     },1000);
 
+    //red light stays on for 3 seconds
     setTimeout(function () {
         green.style.opacity=.3;
         red.style.opacity=1;
         yellow.style.opacity=.3;
     },3000);
 
+    //green light stays on for 8 seconds
     setTimeout(function () {
         green.style.opacity=1;
         red.style.opacity=.3;
@@ -23,6 +25,7 @@ function startTrafficSignal () {
     },8000);
 }
 
+//cycle of traffic lights run for 12 seconds
 let timer=setInterval(function () {
     startTrafficSignal();
 },12000);
